dd40c68204e295be5ffb331d9fc26935dcb80495
ilya <ilyafill@mail.ru>
2026-03-15 20:39:51 +0500

    Update angular to 21

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,10 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { ItskMenuModule } from '@grafit/components';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(ItskMenuModule, BrowserModule, AppRoutingModule), provideHttpClient()],
+  providers: [provideZoneChangeDetection(), importProvidersFrom(ItskMenuModule, BrowserModule, AppRoutingModule), provideHttpClient()],
 }).catch((err) => console.error(err));
